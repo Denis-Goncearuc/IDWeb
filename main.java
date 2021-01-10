ccad0188438cbfb075840fb02233424834ad835c
Denis-Goncearuc <62383541+Denis-Goncearuc@users.noreply.github.com>
2021-01-10 15:52:06 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,26 @@
-class HelloWorld {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(15);
+        list.add(1);
+        list.add(2);
+        list.add(15);
+
+        list.forEach(integer -> {
+            if(!set.contains(integer))
+                set.add(integer);
+            else
+                set.remove(integer);
+        });
+        System.out.println(set);
+
+
     }
-}
