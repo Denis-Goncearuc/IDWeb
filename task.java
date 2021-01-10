a818a3c6b0d5e87dda377648d76abbbb0c8c4ce9
Denis-Goncearuc <62383541+Denis-Goncearuc@users.noreply.github.com>
2021-01-10 15:53:42 +0200

    Create task.java

---
+++ task.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(15);
+        list.add(1);
+        list.add(2);
+        list.add(15);
+
+        list.forEach(integer -> {
+            if(!set.contains(integer))
+                set.add(integer);
+            else
+                set.remove(integer);
+        });
+        System.out.println(set);
+
+
+    }
